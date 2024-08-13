5aa0252df5ce0339a286d995a6e01d2958a877ea
tasbiya02 <tasbiyakumail@gmail.com>
2024-08-13 22:53:12 +0530

    Your commit message

--- src/app/page.js
+++ src/app/page.js
@@ -31,12 +31,20 @@ export default function Home() {
               It is a news website that brings together live news from various sources into one user-friendly platform. Here’s a brief overview of what it offers:
             </p>
         <ul className="list-home">
-          <li><strong>Live News Updates:</strong> The website gathers real-time news from several reputable sources, ensuring that users always have access to the latest information.</li>
+        <li><strong>Live News Updates:</strong> The website gathers real-time news from several reputable sources, ensuring that users always have access to the latest information.</li>
           <li><strong>Smooth Navigation:</strong> The site is designed to handle large amounts of news efficiently, allowing users to browse through articles easily with features like pagination.</li>
           <li><strong>Modern and Responsive Design:</strong> Built with the latest web technologies, the website offers a sleek and modern look that works well on both desktop and mobile devices.</li>
-          <li><strong>Share Button:</strong> Each news article includes a share button, enabling users to easily share content across social media platforms.</li>
-          <li><strong>Voice Assistant Integration(using chrome extension):</strong> Users can navigate the website using voice commands such as "next page or go to next page", "previous page", "refresh", "scroll down", and "scroll up", making it more accessible and interactive(This is not live yet).</li>          
-        </ul> 
+          <li><strong>Share Button:</strong>  Each news article includes a share button, enabling users to easily share content across social media platforms.</li>
+          <li><strong>Category Section:</strong>  Implemented a dynamic category section allowing users to browse news articles by specific categories.</li>
+          <li><strong>Search Functionality:</strong>  Integrated a robust search feature enabling users to find news on specific topics or keywords.</li>
+          <li style={{textDecoration:"underline"}}><strong>Upcoming Enhancements for shamar news:</strong></li>          
+          <ul>
+          <li>Voice Command Navigation:Plan to implement voice command features enabling users to navigate the website with commands such as "next page," "previous page," "refresh," "scroll down," and "scroll up," enhancing accessibility and interactivity.</li>          
+          <li>Infinite Scroll:Integrate an infinite scroll feature to provide a seamless user experience by automatically loading more content as users reach the end of the page.</li>          
+          </ul>
+          
+        </ul>
+        
 
         </div>
         <div className="link-container">
