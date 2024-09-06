7c3fa88bab91f1176cb5d21d7e0294c2c6ad1c7e
tasbiya02 <tasbiyakumail@gmail.com>
2024-09-06 11:01:28 +0530

    changes

--- src/app/api/news/route.js
+++ src/app/api/news/route.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const dynamic = 'force-dynamic';
 export const GET = async (req, res) => {
   try {
-    const response = await axios.get(process.env.BACKEND_API);
+    const response = await axios.get("https://news-insummary.onrender.com/news");
     const data = response.data;
 
     return new Response(JSON.stringify(data), {
